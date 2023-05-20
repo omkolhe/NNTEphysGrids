69c04b83f715a89e3938984aae2be36a1bdf5919
omkolhe <omkolhe026@gmail.com>
2023-05-20 13:36:33 -0400

    WIP: Added state function in Encoder to show rest initiate running

--- PreProcessing/getVelTrigTrials.m
+++ PreProcessing/getVelTrigTrials.m
@@ -1,5 +1,7 @@
 function [Encoder] = getVelTrigTrials(Encoder,windowBeforeTrig,windowAfterTrig,LFPFs)
 
+rejectedTrigs = [];
+
 for i=1:size(Encoder.velTrig,2)
     startTime1 = Encoder.velTrig(1,i)-windowBeforeTrig*Encoder.fs;
     startTime2 = Encoder.velTrig(2,i)-windowBeforeTrig*LFPFs;
@@ -9,7 +11,9 @@ for i=1:size(Encoder.velTrig,2)
         Encoder.trialTime(i,2) = NaN;
         Encoder.trialTime(i,3) = NaN;
         Encoder.trialTime(i,4) = NaN;
+        rejectedTrigs = [rejectedTrigs i];
         continue;
+
     end
     stopTime1 = Encoder.velTrig(1,i)+windowAfterTrig*Encoder.fs - 1;
     stopTime2 = Encoder.velTrig(2,i)+windowAfterTrig*LFPFs - 1;
@@ -19,11 +23,13 @@ for i=1:size(Encoder.velTrig,2)
         Encoder.trialTime(i,2) = NaN;
         Encoder.trialTime(i,3) = NaN;
         Encoder.trialTime(i,4) = NaN;
+        rejectedTrigs = [rejectedTrigs i];
     elseif(mean(Encoder.vel(stopTime1-windowAfterTrig*Encoder.fs:stopTime1+0.0*Encoder.fs))<4)
         Encoder.trialTime(i,1) = NaN;
         Encoder.trialTime(i,2) = NaN;
         Encoder.trialTime(i,3) = NaN;
         Encoder.trialTime(i,4) = NaN;
+        rejectedTrigs = [rejectedTrigs i];
     else
         Encoder.trialTime(i,1) = startTime1;
         Encoder.trialTime(i,2) = stopTime1;
@@ -34,6 +40,7 @@ end
 % Encoder.goodInd = Encoder.goodInd(sum(isnan(Encoder.trialTime),2)==0,:);
 Encoder.trialTime = Encoder.trialTime(sum(isnan(Encoder.trialTime),2)==0,:);
 Encoder.nTrig = size(Encoder.trialTime,1); 
+Encoder.velTrig(:,rejectedTrigs) = [];
 end
 
 
